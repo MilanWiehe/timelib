ae198dd6ce32713b11639ff24e72bba177543390
unknown <milanwiehe@yahoo.de>
2020-02-25 10:48:06 +0100

    Add input_date()

--- main.c
+++ main.c
@@ -9,46 +9,58 @@ int day_of_the_year(int day, int month, int year)
 {
 
     int i = 0;
-    int tageszahlen = 0;
-    int tage[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    int day_number = 0;
+    int day_arr[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
 
-    printf("Tag:");
-    scanf("%i", &day);
+    input_date(&day, &month, &year); //Eingabe von Tag, Monat, Jahr und die Überprüfung.
 
-    printf("Monat:");
-    scanf("%i", &month);
-
-    printf("Jahr:");
-    scanf("%i", &year);
-
-    if(exist_date(day, month, year) == 0) {
-        printf("Dieses Datum ist ungültig");
+    if (is_leapyear(year) == 1) { //Überprüfung ob es sich um ein Schaltjahr handelt.
+        day_arr[2] = 29;
     }
 
-    else {
-
-        if (is_leapyear(year) == 1) {
-            tage[2] = 29;
-        }
-
-        for (i=0; i <= month - 2; i++) {
-            tageszahlen += tage[i];
-        }
-
-        tageszahlen += day;
+    for (i=0; i <= month - 2; i++) { //Ausrechnen der Tagesanzahl bis zum aktuellen Monat.
+        day_number += day_arr[i];
     }
-    return tageszahlen;
+
+    day_number += day; //Hinzufügen der Tage des aktuellen Monats.
+
+    return day_number;
 
     return 0;
 }
 
+
+void input_date(int *day, int *month, int *year)
+{
+    do {
+    printf("Tag:");
+    scanf("%i", &*day);
+    fflush(stdin);
+
+    printf("Monat:");
+    scanf("%i", &*month);
+    fflush(stdin);
+
+    printf("Jahr:");
+    scanf("%i", &*year);
+    fflush(stdin);
+
+    } while(exist_date(*day, *month, *year) == 0);
+
+}
+
+
+
+
+
 int exist_date(int day, int month, int year)
 {
     //int get_days_for_month(day, month, year);
-    int tage[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    int day_arr[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
 
-    if (year > 2400 || get_days_for_month(month, year) == -1 || day > tage[month] || day < 0)
+    if (year > 2400 || get_days_for_month(month, year) == -1 || day > day_arr[month - 1] || day < 0)
     {
+        printf("Dieses Datum ist ungültig");
         return 0;
     }
     else {
@@ -58,17 +70,17 @@ int exist_date(int day, int month, int year)
 }
 
 
+
 int get_days_for_month(int month, int year)
 {
-    int tage[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-    //int is_leapyear(year);
+    int day_arr[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
 
     if (month < 0 || month > 12 || is_leapyear(year) == -1) {
         return -1;
     }
-    else {
 
-        return tage[month];
+    else {
+        return day_arr[month];
     }
 }
 
@@ -76,7 +88,7 @@ int get_days_for_month(int month, int year)
 int is_leapyear(int year)
 {
 
-    int Schaltjahr = 0;
+    int leapyear = 0;
 
     //Überprüfung ob das Jahr durch 4 ohne Rest teilbar ist
     if(year % 4 == 0) {
@@ -84,25 +96,25 @@ int is_leapyear(int year)
         if(year % 100 == 0) {
             //Überprüfung ob das Jahr durch 400 ohne Rest teilbar ist
             if (year % 400 == 0) {
-                Schaltjahr = 1;
+                leapyear = 1;
             }
             else {
-                Schaltjahr = 0;
+                leapyear = 0;
             }
         }
         else {
-            Schaltjahr = 1;
+            leapyear = 1;
         }
     }
     else {
-        Schaltjahr = 0;
+        leapyear = 0;
     }
 
     if (year < 1582) {
-        Schaltjahr = -1;
+        leapyear = -1;
     }
 
-    return Schaltjahr;
+    return leapyear;
 }
 
 
